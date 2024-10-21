3f9614f057f0321312fe91b93e37e01c01833314
nnetr <nnetreba06@gmail.com>
2024-10-22 01:09:14 +0300

    solve 1

--- src/Task1.java
+++ src/Task1.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Task1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        boolean flag = true;
+        while(n != 0){
+            if(flag && n % 10 == 0){
+                n /= 10;
+            } else {
+                System.out.print(n % 10);
+                flag = false;
+                n /= 10;
+            }
+        }
+    }
 }
